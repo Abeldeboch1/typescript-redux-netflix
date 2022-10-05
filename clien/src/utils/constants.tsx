25f1cd04acd92f7256ee8743c1ca23a4043d3ca5
Abeldeboch1 <abeldeboch1@gmail.com>
2022-10-05 17:16:37 -0600

    initial commit

---
+++ clien/src/utils/constants.tsx
@@ -0,0 +1,2 @@
+export const API_KEY = 'e655ce749b6e9689364799559f652abc';
+export const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
---
+++ clien/src/utils/firebase.tsx
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB9jFjrsUHiPY_7O6Y8My95pRH_1Z0bLpw',
+  authDomain: 'typscript-redux-netflix.firebaseapp.com',
+  projectId: 'typscript-redux-netflix',
+  storageBucket: 'typscript-redux-netflix.appspot.com',
+  messagingSenderId: '269144285450',
+  appId: '1:269144285450:web:91001eda796f8befc59af3',
+  measurementId: 'G-YHJLXLJ2HP'
+};
+const app = initializeApp(firebaseConfig);
+export const firebaseAuth = getAuth(app);
